f8d236be53db0a4553d46376eefa434b11a169b2
sandeep singh negi <9041128179sandy@gmail.com>
2022-01-07 00:09:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,28 @@
 const element = document.getElementById('demo');
 const timer = document.getElementById('time');
 
-// setInterval(function() {element.innerHTML = "IST — India Standard Time"}, 1000);
-/*
-setInterval(DisplayHello, 1000);
-function DisplayHello(){
-    document.getElementById('demo').innerHTML = "hello";
-}*/
 
-    const myInterval = setInterval(myTimer, 1000);  
+var globalInterval = setInterval(myTimer, 1000);
 
-    function myTimer(){
+function myTimer() {
     const date = new Date();
     timer.innerHTML = date.toLocaleTimeString();
 }
 
-    function Stop(){
-        console.log('stop');
-       clearInterval(myInterval);
-       clearInterval(obj);
-       
-       
+function Stop() {
+    console.log('stop');
+    clearInterval(globalInterval);
 }
 
-   // let myInterval2 = setInterval(myTimer, 1000);
-    function reset(){
-    // const date = new Date();
-    // timer.innerHTML = date.toLocaleTimeString();
-    //  setInterval(myTimer, 1000);
-        createSetInterval();
-   console.log("reset");
+// let myInterval2 = setInterval(myTimer, 1000);
+function reset() {
+ 
+    globalInterval = createSetInterval();
+    console.log("reset");
+
 }
 
-function createSetInterval(){
+function createSetInterval() {
     const obj = setInterval(myTimer, 1000);
     return obj;
 
